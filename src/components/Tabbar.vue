1b819c86dbb8d2df54237c9e964ea54fe1383fb6
startee <startee@github>
2023-09-21 22:37:34 +0800

    update Tabbar.vue

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const other_show = ref<boolean>(false)
+</script>
+
 <template>
   <div class="tabbar">
     <div class="list">
@@ -13,10 +19,18 @@
       </router-link>
     </div>
     <div class="list">
-      <router-link to="/">
-        <img src="@/preview/image/Birds.png"/>
-        <div class="title">其他</div>
-      </router-link>
+      <img src="@/preview/image/Birds.png" @click="other_show = !other_show"/>
+      <div class="title" @click="other_show = !other_show">其他</div>
+      <div class="more-list" v-if="other_show">
+        <div>红包</div>
+        <div>代金券</div>
+        <div>津贴</div>
+        <div>收藏</div>
+        <div>企业用餐</div>
+        <div>发票助手</div>
+        <div>餐具设置</div>
+        <div>管理地址</div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,6 +52,7 @@
   box-sizing: border-box;
   height: 50px;
   text-align: center;
+  position: relative;
   img {
     width: 30px;
     height: 30px;
@@ -46,5 +61,30 @@
   .title {
     font-size: 15px;
   }
+}
+.more-list {
+  position: absolute;
+  top: -350px;
+  left: -20px;
+  width: 100px;
+  background-color: #fff;
+  border: 1px solid var(--color-text-grey);
+  border-radius: 5px;
+  padding: 0 10px;
+  & > div {
+    padding: 10px;
+    border-bottom: 1px solid var(--color-text-grey);
+    text-align: center;
+  }
+}
+.more-list::after {
+  content: "";
+  position: absolute;
+  bottom: -16px;
+  transform: translateX(-50%);
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+  border-top: 8px solid var(--color-text-grey);
+  border-bottom: 8px solid transparent;
 }
 </style>
--- src/views/waimai/index/index.vue
+++ src/views/waimai/index/index.vue
@@ -148,6 +148,7 @@ section {
 }
 footer {
   position: absolute;
+  left: 0;
   bottom: 0;
   width: 100%;
 }
